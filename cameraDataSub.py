0de6d9b6ac3021661081caf626c298adc34ac17f
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2025-07-02 13:33:14 -0400

    Create cameraDataSub.py

---
+++ cameraDataSub.py
@@ -0,0 +1,10 @@
+import zmq
+context = zmq.Context()
+sub = context.socket(zmq.SUB)
+sub.connect("tcp://192.168.149.1:5555")
+sub.setsockopt_string(zmq.SUBSCRIBE, "")
+
+print("Starting subscriber")
+while True:
+    image = sub.recv()
+    print(image)
