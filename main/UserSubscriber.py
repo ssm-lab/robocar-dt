26803e5a7e3c005d4e49ec630560aad93fee4c6a
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2025-07-16 12:36:25 -0400

    Reduced lag due to object detection

--- main/UserSubscriber.py
+++ main/UserSubscriber.py
@@ -1,34 +1,57 @@
-import imagezmq
 import cv2
+import time
+import imagezmq
 import numpy as np
-
+from ultralytics import YOLO
 __author__ = "Adwita Kashyap"
 __credits__ = "Istvan David"
 
 """
 User subscriber: 
 - Runs on the user's device
-- Receives data from CarPublisher.py
+- Receives images from CarPublisher.py
+- Runs object detection using YOLO on received images
 """
 
 class UserSubscriber():
 
     def __init__(self):
         self.imageHub = imagezmq.ImageHub(open_port='tcp://192.168.149.1:5557', REQ_REP = False)
+        self.startTime = time.time()
         
     def subscribe(self):
         running = True
+        framesToSkip = 0
+        fps = 30 ## from hardware stats
+        allowedTime = 1/fps # Time allowed for processing each frame
+
+        path = "C:/Users/adwit/runs/detect/train2/weights/best.pt"
+        model = YOLO(path)
+
         while running:
-            title, buffer = self.imageHub.recv_jpg() 
+            for i in range (framesToSkip + 1):
+                timestamp, buffer = self.imageHub.recv_jpg() 
+
+            startTime = time.time()
+
             buffer = bytes(buffer)
             image = cv2.imdecode(np.frombuffer(buffer, dtype=np.uint8), cv2.IMREAD_COLOR)
-            cv2.imshow(title, image)
+            results = model.predict(image)[0]
+            image = results.plot()
+            cv2.imshow("live", image)
+
+            elapsedTime = time.time() - startTime
+
+            ## Find number of frames to skip
+            if elapsedTime> allowedTime:
+                framesToSkip = int((elapsedTime - allowedTime) * fps) 
+            else:
+                framesToSkip = 0
 
             # Press any key to exit
             if cv2.waitKey(1) > -1:
                 running = False
-            
-        print("done")
+                
         cv2.destroyAllWindows()
 
 
